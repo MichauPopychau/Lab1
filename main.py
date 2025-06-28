f5e186c2b446cfdc16d3382347d23eb5fd228766
Michał Nosko <wrx86636@student.wroclaw.merito.pl>
2025-06-28 11:17:34 +0200

    Dodano menu nawigacyjne w gałęzi feature/navigation

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def menu():
+    print("Menu nawigacyjne:")
+    print("1. Strona główna")
+    print("2. O projekcie")
+    print("3. Wyjście")
+
+menu()
+
 print("Hello world")
 
 print("How are you")
