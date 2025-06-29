54f7787adf38acdd574fa29a2dd8319931163aa4
Michał Nosko <wrx86636@student.wroclaw.merito.pl>
2025-06-29 14:54:24 +0200

    Dodano kalkulator

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+import unittest
+from calculator import dodaj, odejmij
+
+class TestCalculator(unittest.TestCase):
+    def test_dodaj(self):
+        self.assertEqual(dodaj(2, 3), 5)
+        self.assertEqual(dodaj(-1, 1), 0)
+        self.assertEqual(dodaj(0, 0), 0)
+
+    def test_odejmij(self):
+        self.assertEqual(odejmij(5, 3), 2)
+        self.assertEqual(odejmij(0, 0), 0)
+        self.assertEqual(odejmij(-1, -1), 0)
+
+    def test_dodaj_i_odejmij(self):
+        result = odejmij(dodaj(10, 5), 3)
+        self.assertEqual(result, 12)
